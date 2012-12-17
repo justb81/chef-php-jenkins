af5e89b99ca67d6c2e6ac887c9c4388354e7ad31
Anusch Athari <anusch@athari.de>
2012-12-17 20:23:08 +0100

    Fixed some violations of foodcritic rules

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,31 +10,31 @@ php_pear " pear.netpirates.net/phpDox" do
 end
 
 #workaround for https://github.com/fnichol/chef-jenkins/issues/9
-directory "#{node[:jenkins][:server][:home]}/updates/" do
-  owner "#{node[:jenkins][:server][:user]}"
-  group "#{node[:jenkins][:server][:user]}"
+directory "#{node['jenkins']['server']['home']}/updates/" do
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
   action :create
 end
 
 execute "update jenkins update center" do
-  command "wget http://updates.jenkins-ci.org/update-center.json -qO- | sed '1d;$d'  > #{node[:jenkins][:server][:home]}/updates/default.json"
-  user "#{node[:jenkins][:server][:user]}"
-  group "#{node[:jenkins][:server][:user]}"
-  creates "#{node[:jenkins][:server][:home]}/updates/default.json"
+  command "wget http://updates.jenkins-ci.org/update-center.json -qO- | sed '1d;$d'  > #{node['jenkins']['server']['home']}/updates/default.json"
+  user node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
+  creates "#{node['jenkins']['server']['home']}/updates/default.json"
 end
 
 jenkins_cli "install-plugin checkstyle cloverphp dry htmlpublisher jdepend plot pmd violations xunit git"
 
-directory "#{node[:jenkins][:server][:home]}/jobs/php-template/" do
-  owner "#{node[:jenkins][:server][:user]}"
-  group "#{node[:jenkins][:server][:user]}"
+directory "#{node['jenkins']['server']['home']}/jobs/php-template/" do
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
   action :create
 end
 
-template "#{node[:jenkins][:server][:home]}/jobs/php-template/config.xml" do
+template "#{node['jenkins']['server']['home']}/jobs/php-template/config.xml" do
   source "config.xml"
-  owner "#{node[:jenkins][:server][:user]}"
-  group "#{node[:jenkins][:server][:user]}"
+  owner node['jenkins']['server']['user']
+  group node['jenkins']['server']['user']
   mode "0644"
 end
 
