0e72fae57034570ef2fd9a42321473678f55090c
Anusch Athari <anusch@athari.de>
2012-12-17 20:32:25 +0100

    Added cookbook metadata

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name 'php-jenkins'
+description 'Installs and configures jenkins for use with php as described here: http://jenkins-php.org/'
+maintainer 'Anusch Athari'
+maintainer_email 'anusch@athari.de'
+
+licensing 'CC0 1.0 Universal - Public Domain'
+
+depends 'java'
+depends 'ant'
+depends 'jenkins'
+depends 'php'
+
